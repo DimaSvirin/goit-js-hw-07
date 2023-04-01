68a3781d4e0c14db1e11961a8941741e234b349b
DimaSvirin <dimasvirin@gmail.com>
2023-04-01 14:59:26 +0300

    add gallery item element

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.createElement("li")
+galleryList.classList.add("gallery__item")
 
+// Шаблон
+// <li class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </li>
 console.log(galleryItems);
