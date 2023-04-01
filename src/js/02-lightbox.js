85255ab82f24b70375f2974bae1cd47fabd9d49d
DimaSvirin <dimasvirin@gmail.com>
2023-04-01 16:52:03 +0300

    add scripts to html

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery")
+
+function createGalleryCards(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+    `;
+    }).join("");
+};
+
+const cardsMarkup = createGalleryCards(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
 
 console.log(galleryItems);
